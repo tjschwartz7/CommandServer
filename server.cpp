be34fcfa3ed303d7dc30b909c128b99a96b9ffb8
tj <tj.schwartz2000@gmail.com>
2024-03-31 09:27:52 -0400

    Cleaned up code by separating functions from server logic. Accept was in the main server loop when it shouldn't have been, so it disconnected the client each time it sent a message. Resolved client ticket issue.

--- server.cpp
+++ server.cpp
@@ -26,6 +26,9 @@ Environment is a linux docker container. Thats how we get away with
 using all of these linux includes.
 */
 
+std::string CheckAccess(std::string message, SecureAccess& schema);
+std::string EditAccess(std::string message, SecureAccess& schema, bool addAccess);
+
 int main() 
 {
     SecureAccess schema(10000);
@@ -66,121 +69,45 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    std::cout << "Listening for connection on port " << PORT << std::endl;
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    //We have a session
     bool done = false;
     while(!done)
     {
-        std::cout << "Listening for connection on port " << PORT << std::endl;
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
         valread = read( new_socket , buffer, 1024);
 
         std::string client_message = buffer;
         std::string send_buf = "Invalid argument. Please try again.";
         int index = client_message.find_first_of(" ");
+        std::string word = client_message.substr(0, index);
+        client_message = client_message.substr(index+1);
 
         //Not end of string, and the space isnt the final character in our string
-        if(index != std::string::npos && index != client_message.length() - 1)
+        //All commands in this block have arguments
+        bool commandHasArgs = index != std::string::npos && index != client_message.length() - 1;
+        if(commandHasArgs)
         {
-            int user;
-            int group;
-            bool AddAccess = false;
-            std::string word = client_message.substr(0, index);
+            bool addAccess = false;
             
             //If we're adding access, we want to pass 'true' along in our EditAccess argument
-            if(word == "AddAccess") AddAccess = true;
+            if(word == "AddAccess") addAccess = true;
             
             //Edit Access case
             if(word == "AddAccess" ||
                word == "RemoveAccess")
             {
-                //Remove the first word from client_message
-                client_message = client_message.substr(index+1);
-
-                //Find index of next space
-                index = client_message.find_first_of(" ");
-                if(index != std::string::npos && index != client_message.length() - 1)
-                {
-                    //Next word should be an integer
-                    word = client_message.substr(0, index);
-                    int conversion = std::stoi(word);
-
-                    //Checking to make sure stoi didn't return zero due to invalid input
-                    //We're expecting the user number
-                    if(std::to_string(conversion) == word)
-                    {
-                        //User number is this conversion
-                        user = conversion;
-                        //Remove last word from client_message
-                        client_message = client_message.substr(index+1);
-                        index = client_message.find_first_of(" ");
-
-                        //Next word should be another integer - the group number
-                        word = client_message.substr(0, index);
-                        conversion = std::stoi(word);
-
-                        //We're expecting the group number
-                        //If we convert the number back to a string and it equals word...
-                        //It was a valid input
-                        if(std::to_string(conversion) == word)
-                        {
-                            group = conversion;
-                            schema.EditAccess(user, group, AddAccess);
-                            if(AddAccess)
-                                send_buf = "User " + std::to_string(user) + " has been granted access to group " + std::to_string(group) + "\n";
-                            else 
-                                send_buf = "User " + std::to_string(user) + " has lost access to group " + std::to_string(group) + "\n";
-                        }   
-                        
-                    }
-                }
+                send_buf = EditAccess(client_message, schema, addAccess);
                 
             }
             else if(word == "CheckAccess")
             {
-                //Remove the first word from client_message
-                client_message = client_message.substr(index+1);
-
-                //Find index of next space
-                index = client_message.find_first_of(" ");
-                if(index != std::string::npos && index != client_message.length() - 1)
-                {
-                    //Next word should be an integer
-                    word = client_message.substr(0, index);
-                    int conversion = std::stoi(word);
-
-                    //Checking to make sure stoi didn't return zero due to invalid input
-                    //We're expecting the user number
-                    if(std::to_string(conversion) == word)
-                    {
-                        //User number is this conversion
-                        user = conversion;
-                        //Remove last word from client_message
-                        client_message = client_message.substr(index+1);
-                        index = client_message.find_first_of(" ");
-
-                        //Next word should be another integer - the group number
-                        word = client_message.substr(0, index);
-                        conversion = std::stoi(word);
-
-                        //We're expecting the group number
-                        //If we convert the number back to a string and it equals word...
-                        //It was a valid input
-                        if(std::to_string(conversion) == word)
-                        {
-                            group = conversion;
-                            bool hasAccess = schema.UserHasAccess(user, group);
-                            if(hasAccess)
-                                send_buf = "User " + std::to_string(user) + " has access to group " + std::to_string(group) + "\n";
-                            else 
-                                send_buf = "User " + std::to_string(user) + " does not have access to group " + std::to_string(group) + "\n";
-                        }   
-                        
-                    }
-                }
-                
+                send_buf = CheckAccess(client_message, schema);
             }
         }
         else if(word == "Quit")
@@ -195,4 +122,101 @@ int main()
         std::cout << "Message sent to client." << std::endl;
     }
     return 0;
+}
+
+
+std::string CheckAccess(std::string message, SecureAccess& schema)
+{
+    int user;
+    int group;
+    std::string word;
+    std::string ret_string = "";
+    //Remove the first word from client_message
+    
+
+    //Find index of next space
+    int index = message.find_first_of(" ");
+    if(index != std::string::npos && index != message.length() - 1)
+    {
+        //Next word should be an integer
+        word = message.substr(0, index);
+        int conversion = std::stoi(word);
+
+        //Checking to make sure stoi didn't return zero due to invalid input
+        //We're expecting the user number
+        if(std::to_string(conversion) == word)
+        {
+            //User number is this conversion
+            user = conversion;
+            //Remove last word from client_message
+            message = message.substr(index+1);
+            index = message.find_first_of(" ");
+
+            //Next word should be another integer - the group number
+            word = message.substr(0, index);
+            conversion = std::stoi(word);
+
+            //We're expecting the group number
+            //If we convert the number back to a string and it equals word...
+            //It was a valid input
+            if(std::to_string(conversion) == word)
+            {
+                group = conversion;
+                bool hasAccess = schema.UserHasAccess(user, group);
+                if(hasAccess)
+                    ret_string = "User " + std::to_string(user) + " has access to group " + std::to_string(group) + "\n";
+                else 
+                    ret_string = "User " + std::to_string(user) + " does not have access to group " + std::to_string(group) + "\n";
+            }   
+            
+        }
+    }           
+    return ret_string;
+}
+
+std::string EditAccess(std::string message, SecureAccess& schema, bool addAccess)
+{
+    int user;
+    int group;
+    std::string word;
+    std::string ret_string = "";
+    //Remove the first word from client_message
+
+    //Find index of next space
+    int index = message.find_first_of(" ");
+    if(index != std::string::npos && index != message.length() - 1)
+    {
+        //Next word should be an integer
+        word = message.substr(0, index);
+        int conversion = std::stoi(word);
+
+        //Checking to make sure stoi didn't return zero due to invalid input
+        //We're expecting the user number
+        if(std::to_string(conversion) == word)
+        {
+            //User number is this conversion
+            user = conversion;
+            //Remove last word from client_message
+            message = message.substr(index+1);
+            index = message.find_first_of(" ");
+
+            //Next word should be another integer - the group number
+            word = message.substr(0, index);
+            conversion = std::stoi(word);
+
+            //We're expecting the group number
+            //If we convert the number back to a string and it equals word...
+            //It was a valid input
+            if(std::to_string(conversion) == word)
+            {
+                group = conversion;
+                schema.EditAccess(user, group, addAccess);
+                if(addAccess)
+                    ret_string = "User " + std::to_string(user) + " has been granted access to group " + std::to_string(group) + "\n";
+                else 
+                    ret_string = "User " + std::to_string(user) + " has lost access to group " + std::to_string(group) + "\n";
+            }   
+        }
+    }
+    return ret_string;
 }
